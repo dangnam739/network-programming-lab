d4c3e0a7f7a1eb58be37bc75a689b57a85271798
dangnam739 <47108512+dangnam739@users.noreply.github.com>
2020-10-06 23:14:40 +0700

    add ex1 for lab2

---
+++ 2.TCP_Socket/ex1/echoServer.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define MAXLINE 4096 /*max text line length*/
+#define SERV_PORT 3000 /*port*/
+#define LISTENQ 8 /*maximum number of client connections */
+
+int main (int argc, char **argv)
+{
+ int listenfd, connfd, n;
+ pid_t childpid;
+ socklen_t clilen;
+ char buf[MAXLINE];
+ struct sockaddr_in cliaddr, servaddr;
+
+ //creation of the socket
+ listenfd = socket (AF_INET, SOCK_STREAM, 0);
+
+ //preparation of the socket address
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ servaddr.sin_port = htons(SERV_PORT);
+
+ bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+ listen (listenfd, LISTENQ);
+
+ printf("%s\n","Server running...waiting for connections.");
+
+ for ( ; ; ) {
+
+  clilen = sizeof(cliaddr);
+  connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+  printf("%s\n","Received request...");
+
+  if(connfd > 0){
+    printf("Client ip connected: %s\n", inet_ntoa(cliaddr.sin_addr));
+    printf("Client port connected: %d\n", ntohs(cliaddr.sin_port));
+  }
+
+  while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
+   printf("%s","String received from and resent to the client:");
+   puts(buf);
+   strcpy(buf, "Monday");
+   send(connfd, buf, n, 0);
+  }
+
+ if (n < 0) {
+  perror("Read error");
+  exit(1);
+ }
+ close(connfd);
+
+ }
+ //close listening socket
+ close (listenfd);
+}
